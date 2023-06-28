d94fdd57da8ba815a3495a272a99e63b866994c4
Olga Boris <olya_boris97@mail.ru>
2023-06-28 23:24:39 +0300

    fix

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,22 @@
 
 $(document).ready(function(){
-    $('#slider').owlCarousel();
+    $('#slider').owlCarousel({
+
+        loop: true,
+        nav: true,
+        margin: 40,
+        dots: true,
+        autoplay: false,
+        navText: true,
+        stopOnHover: true,
+        smartSpeed: 1000, //Время движения слайда
+        autoplayTimeout: 4000, //Время смены слайда
+        pagination: true,
+        responsiveClass: true,
+        responsive: {
+            1200: {
+                items: 3
+            }
+        }
+    });
 });
